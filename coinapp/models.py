4b7b58aa9d6e7ea8d2c8212ded2e653d2019bc41
PranavPookuth <pranavpookuth@gmail.com>
2024-11-10 20:04:02 +0530

    first commit

--- coinapp/models.py
+++ coinapp/models.py
@@ -105,7 +105,7 @@ class HotelBooking(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_applied = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     points_used = models.PositiveIntegerField(default=0)
-    booking_date = models.DateTimeField(auto_now_add=True)
+    booking_date = models.DateTimeField(auto_now_add=True )
     def __str__(self):
         return f"Booking by {self.name.email} at {self.hotel.name} for {self.number_of_rooms} rooms on{self.booking_date}"
 
